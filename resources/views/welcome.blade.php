a23b69aadf14ef32cfb0cf3aa59f23128a8c6656
elinaldoA <83353178+elinaldoA@users.noreply.github.com>
2023-09-20 16:27:05 -0300

    Logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                Sistemas EA
+                Sistemas <sup>EA</sup>
             </div>
 
             <div class="links">
